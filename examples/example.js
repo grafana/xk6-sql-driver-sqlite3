fb727e2529dbc2c5522612f24161c3b2487e484b
Iván SZKIBA <szkiba@users.noreply.github.com>
2024-10-21 18:34:28 +0200

    feat: upgrade to latest xk6-sql: query function is now part of the Database interface

--- examples/example.js
+++ examples/example.js
@@ -4,10 +4,14 @@ import driver from "k6/x/sql/driver/sqlite3";
 const db = sql.open(driver, "./test.db");
 
 export function setup() {
-  db.exec(`CREATE TABLE IF NOT EXISTS keyvalues (
-           id integer PRIMARY KEY AUTOINCREMENT,
-           key varchar NOT NULL,
-           value varchar);`);
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS roster
+      (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        given_name VARCHAR NOT NULL,
+        family_name VARCHAR NOT NULL
+      );
+  `);
 }
 
 export function teardown() {
@@ -15,10 +19,19 @@ export function teardown() {
 }
 
 export default function () {
-  db.exec("INSERT INTO keyvalues (key, value) VALUES('plugin-name', 'k6-plugin-sql');");
+  let result = db.exec(`
+    INSERT INTO roster
+      (given_name, family_name)
+    VALUES
+      ('Peter', 'Pan'),
+      ('Wendy', 'Darling'),
+      ('Tinker', 'Bell'),
+      ('James', 'Hook');
+  `);
+  console.log(`${result.rowsAffected()} rows inserted`);
 
-  let results = sql.query(db, "SELECT * FROM keyvalues WHERE key = $1;", "plugin-name");
-  for (const row of results) {
-    console.log(`key: ${row.key}, value: ${row.value}`);
+  let rows = db.query("SELECT * FROM roster WHERE given_name = $1;", "Peter");
+  for (const row of rows) {
+    console.log(`${row.family_name}, ${row.given_name}`);
   }
 }
